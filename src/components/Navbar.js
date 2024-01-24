7d98b364adf8214312b0715a374fb29e0aeb71b3
dylstar135 <dylstar160990@googlemail.com>
2024-01-24 10:36:43 +0000

    styling for fonts and navbar added, button styliing created for when component is built

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,20 @@ const Navbar = () => {
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
