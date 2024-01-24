127c23b4168daf472d8cdd418c89671fa8bb6196
dylstar135 <dylstar160990@googlemail.com>
2024-01-24 11:38:04 +0000

    media queries added to make the navbar responive to screen sizes, struggled with concept,but after some time recapping managed to ge the hang of it. if else statements used for the hamburger menu to make sure a cross appears to get rid of the menu when necessary. transition to menu added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import "./NavbarStyles.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -23,6 +26,13 @@ const Navbar = () => {
           <Link to="/Contact">Contact</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
+      </div>
     </div>
   );
 };
