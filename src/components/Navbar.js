eb04f150312688c80bdc72bd5ff0d053d25ef354
dylstar135 <dylstar160990@googlemail.com>
2024-01-24 10:08:53 +0000

    start of the portfolio mk1, pages created, files structured, nav bar started

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./NavbarStyles.css";
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
