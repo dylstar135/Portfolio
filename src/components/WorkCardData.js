0ee66cc7e9282b25b6149a085c791845899e1bb9
dylstar135 <dylstar160990@googlemail.com>
2024-01-25 15:26:54 +0000

    packer projects added

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,17 +1,24 @@
 import Pro1 from "../assets/Pro1.png";
 import projectquote from "../assets/project-quote.png";
+import PilgrimPack from "../assets/PilgrimPack.png";
 
 const ProjectCardData = [
   {
     imgsrc: Pro1,
     title: "Portfolio",
-    text: "My first built Portfolio",
+    text: "My first built Portfolio.",
     view: "https://dylan-power-dev.netlify.app/",
   },
   {
     imgsrc: projectquote,
     title: "Random Quote Generator",
-    text: "Inspire yourself with the click of a button",
+    text: "Inspire yourself with the click of a button.",
+    view: "https://inspired-quotes.netlify.app/",
+  },
+  {
+    imgsrc: PilgrimPack,
+    title: "Pilgrims Packer",
+    text: "Gear packing list  built out of my own love for adventure, and my exceptional skill at forgetting things.",
     view: "https://inspired-quotes.netlify.app/",
   },
 ];
